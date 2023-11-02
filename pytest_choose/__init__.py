b25071c6aeaaeec7244ec59ac910d423752b8162
Azusa <1049798454@qq.com>
2023-11-02 11:02:45 +0800

    release.版本号提升

--- pytest_choose/__init__.py
+++ pytest_choose/__init__.py
@@ -4,5 +4,5 @@
 @Description: 
 
 """
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __plugin_name__ = 'pytest-choose'
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name=PACKAGE,
-    version="0.0.3",
+    version="0.0.4",
     keywords="pytest",
     author="Azusa",
     author_email="naotosaka@126.com",
