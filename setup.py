ae75c92f01d3fbb3fd20fa14fabbed184f2527c0
Azusa <1049798454@qq.com>
2023-12-26 17:02:54 +0800

    release.版本号提升

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name=PACKAGE,
-    version="0.0.7",
+    version="0.1.0",
     keywords="pytest",
     author="Azusa",
     author_email="naotosaka@126.com",
