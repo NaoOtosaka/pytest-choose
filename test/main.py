ec52c6ec8cf28d786ce51d15fb4cdc3002068ac9
Azusa <1049798454@qq.com>
2023-12-26 14:56:54 +0800

    feat.测试文件更新

--- test/main.py
+++ test/main.py
@@ -6,4 +6,13 @@
 """
 import pytest
 
-pytest.main(['--fc', 'on'])
+pytest.main(
+    [
+        '--fc', 'on',
+        '--fc-allow-path', './choose.json',
+        '--fc-allow-path', './choose_1.json',
+        '--fc-allow-path', './choose_2.json',
+        '--fc-block-path', './filter.json',
+        '--fc-block-path', './filter_1.json',
+    ]
+)
