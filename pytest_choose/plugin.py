1aa41984e403cdb3350c0bd677951177103980d7
Azusa <1049798454@qq.com>
2024-01-31 16:37:53 +0800

    feat.支持定义path及marker规则

--- pytest_choose/plugin.py
+++ pytest_choose/plugin.py
@@ -43,6 +43,17 @@ def pytest_addoption(parser: pytest.Parser):
     )
 
 
+def pytest_load_initial_conftests(early_config: pytest.Config):
+    if getattr(early_config.known_args_namespace, 'fc') == 'on':
+        allow_list_path = getattr(early_config.known_args_namespace, 'fc_allow_path')
+        coding = getattr(early_config.known_args_namespace, 'fc_coding')
+        allow_list_parse = ChooseFileAnalysis(allow_list_path, None, encoding=coding).parse()
+        if new_path := allow_list_parse['path']:
+            setattr(early_config.option, 'file_or_dir', new_path)
+        if new_marker := allow_list_parse['marker']:
+            setattr(early_config.option, 'markexpr', new_marker)
+
+
 def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config, items: List["pytest.Item"]):
     if config.getoption('--fc') == 'on':
         origin_count = len(items)
@@ -60,6 +71,7 @@ def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config
                 del items[items.index(item)]
                 count += 1
             terminal_write(session, f'Filter {count} cases and collect {origin_count - count} cases', bold=True)
+            terminal_write(session, f'Use marker: {allow_list_parse["marker"]}')
         else:
             terminal_write(session, 'Unsupported file format, please use JSON format file', red=True, bold=True)
             terminal_write(session, 'Not filtered')
